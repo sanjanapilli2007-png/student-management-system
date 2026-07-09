084cf567903941b8a6de968965625c36c67f0c04
sanjanapilli2007-png <sanjanapilli2007@gmail.com>
2026-07-09 10:43:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-elif choice == "1":
-    name = input("Enter Student Name: ")
-    roll = input("Enter Roll Number: ")
-    department = input("Enter Department: ")
-
-    student = {
-        "Name": name,
-        "Roll": roll,
-        "Department": department
-    }
-
-    students.append(student)
-
-    with open("students.txt", "a") as file:
-        file.write(f"{name},{roll},{department}\n")
-
-    print("Student added successfully!")
+Add two students
+view the students
+search for one roll number
+update a students details
+delete a student
+exit the program
