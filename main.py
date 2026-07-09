83930fd05f470aeb93a0a054622d4c80655fd1ca
sanjanapilli2007-png <sanjanapilli2007@gmail.com>
2026-07-09 10:28:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-students = []
-
-while True:
-    print("\n===== Student Management System =====")
-    print("1. Add Student")
-    print("2. View Students")
-    print("3. Search Student")
-    print("4. Update Student")
-    print("5. Delete Student")
-    print("6. Exit")
-
-    choice = input("Enter your choice: ")
-
-    if choice == "1":
-        name = input("Enter Student Name: ")
-        roll = input("Enter Roll Number: ")
-        department = input("Enter Department: ")
-
-        student = {
-            "Name": name,
-            "Roll": roll,
-            "Department": department
-        }
-
-        students.append(student)
-        print("Student added successfully!")
 
     elif choice == "2":
         if len(students) == 0:
@@ -33,9 +7,4 @@ while True:
             for student in students:
                 print(student)
 
-    elif choice == "6":
-        print("Thank you!")
-        break
-
-    else:
-        print("This feature will be added later.")
+    
