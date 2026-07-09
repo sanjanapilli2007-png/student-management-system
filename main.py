60e77b8fcf6c2b1ba40fbdf86045f63b9bd8d189
sanjanapilli2007-png <sanjanapilli2007@gmail.com>
2026-07-09 10:22:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-print("Welcome to Student Management System")
-
-while True:
-    print("\n===== MENU =====")
-    print("1. Add Student")
-    print("2. View Students")
-    print("3. Search Student")
-    print("4. Update Student")
-    print("5. Delete Student")
-    print("6. Exit")
-
-    choice = input("Enter your choice: ")
-
-    if choice == "6":
-        print("Thank you!")
-        break
-    else:
-        print("Feature will be added soon.")
+Project Planning → Done
+↓
+Create GitHub Repository → Done
+↓
+Design Program Flow → In Progress
+↓
+Edit main.py and commit the menu code
+↓
+Design Program Flow → Done
+↓
+Write Add Student Function → In Progress
