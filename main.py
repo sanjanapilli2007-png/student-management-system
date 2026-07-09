3352113a48764d4995f2169a1f02719780c953a7
sanjanapilli2007-png <sanjanapilli2007@gmail.com>
2026-07-09 10:36:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,18 @@ elif choice == "3":
 
     if not found:
         print("Student not found.")
+        elif choice == "4":
+    roll = input("Enter Roll Number to update: ")
+
+    found = False
+    for student in students:
+        if student["Roll"] == roll:
+            student["Name"] = input("Enter New Name: ")
+            student["Department"] = input("Enter New Department: ")
+            print("Student record updated successfully!")
+            found = True
+            break
+
+    if not found:
+        print("Student not found.")
     
