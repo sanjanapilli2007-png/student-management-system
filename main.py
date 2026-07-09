2f4e796139b3864c5ccb3fdbe7fec4504b1e24bf
sanjanapilli2007-png <sanjanapilli2007@gmail.com>
2026-07-09 10:08:47 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Welcome to Student Management System")
+
+while True:
+    print("\n===== MENU =====")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Update Student")
+    print("5. Delete Student")
+    print("6. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "6":
+        print("Thank you!")
+        break
+    else:
+        print("Feature will be added soon.")
